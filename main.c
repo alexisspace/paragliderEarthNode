8fa2f7ad2d87c752bbd7f34e27049e958f9d9e97
alexisspace <alexis.guillermo.ortega.acurero@gmail.com>
2016-08-30 13:00:47 -0400

    Ready to test with MATLAB

--- main.c
+++ main.c
@@ -1,59 +1,47 @@
-
-
 #include <p33FJ12GP202.h>
 #include <i2c.h>
 #include <libq.h>
 #include <dsp.h>
 #include <string.h>
 #include <spi.h>
-// the above include path may be different for each user.  If a compile
-// time error appears then check the path for the file above and edit
-// the include statement above.
+#include "nRF24L01p.h"
+
 
 //#define XTFREQ          7370000         //FRC frequency 
 #define FCY             39613000        //Instruction Cycle Frequency
-
 #define BAUDRATE         9600		      
 #define BRGVAL          ((FCY/BAUDRATE)/16)-1 
 
-
-// Registros del nRF24L01+
-
-
-
-
-#define N_SAMPLE_BYTES		14
-#define USART_TX_BUFFER_SIZE	10
-#define SPI_BUFFER_SIZE	10
-
-
-
 // Constantes de la implementacion
 #define DEG2RAD				17.4533e-003
-
-
-
+#define USART_TX_BUFFER_SIZE	11
+#define SPI_BUFFER_SIZE	11
+#define BYTES_TO_READ 10
 
 // Variables globales
-unsigned char SPI_buffer[SPI_BUFFER_SIZE], U1TX_buffer[USART_TX_BUFFER_SIZE];
+char SPI_buffer[SPI_BUFFER_SIZE], U1TX_buffer[USART_TX_BUFFER_SIZE], *SPI_buffer_ptr;
 unsigned char cmd, U1TX_byte_counter;
-unsigned char SPI_byte_counter, SPI_max_bytes, *SPI_buffer_ptr;
-struct status
-{
-   char status;
-   unsigned char data;
-}; // Se define el tipo "struct status"
-typedef struct status STATUS; // Se abrevia STATUS
-STATUS UART_status, SPI_status; // Se definen variables de tipo STATUS
+unsigned char SPI_byte_counter, SPI_max_bytes;
 
+typedef struct status {
+   char status;
+   char data;
+   }; // Se define el tipo "struct status"
+//typedef struct status STATUS; // Se abrevia STATUS
+
+volatile struct status UART_status = {0,0};
+volatile struct status SPI_status = {0,0}; // Estado del SPI
+volatile struct status SYS_status = {0,0}; // Estado del sistema
 
 // Prototipos de funciones
 char UARTbufferSend(char* c_ptr);
-char SPI_WriteCmd(char reg, char * d_ptr, unsigned char n_data);
-char SPI_ReadCmd(char reg, char * d_ptr, unsigned char n_data);
+char SPI_WriteAddr(unsigned char cmd, unsigned char addr, char * d_ptr, unsigned char n_data);
+char SPI_ReadAddr(unsigned char cmd, unsigned char addr, char * d_ptr, unsigned char n_data);
+void startRF_TXRX(void);
 
 int main(void)
 {
+   unsigned char cmd, sys_cmd, addr, n_bytes, k; 
 	unsigned int config1, config2, config3;
 
 // Configure Oscillator to operate the device at 40Mhz
@@ -72,7 +60,7 @@ int main(void)
 // Configurar pines
 	RPINR18 = 9;			// Make Pin RP9 U1RX
 	RPOR4bits.RP8R = 3;		// Make Pin RP8 U1TX
-	RPIN20 = 0x01;          // SDI assigned to RP1
+	RPINR20 = 0x01;          // SDI assigned to RP1
 	RPOR1bits.RP2R = 0X07;  // SDO assigned to RP2
 	RPOR1bits.RP3R = 0X08;  // SCK assigned to RP3            
 	AD1PCFGL = 0x03C0;		// Make analog pins digital
@@ -90,12 +78,13 @@ int main(void)
 	U1MODEbits.UARTEN = 1; // Enable UART
 	U1STAbits.UTXEN = 1; // Enable UART TX
 	IEC0bits.U1TXIE = 0; // Disable UART TX interrupt
+	IEC0bits.U1RXIE = 1; // Enable RX interrupt
 
 // Configurar interrupcion externa INT0 (RB7, PIN16)
 	INTCON2bits.INT0EP = 1;		// Interrupcion en 0:flanco ascendente 1: flanco descendente
 	IFS0bits.INT0IF = 0;		// Clear flag
-//	IEC0bits.INT0IE = 1;		// Habilitar interrupcion
 	CNPU2bits.CN23PUE = 0;	// pull-up on RB7: 0 = Disable; 1 = Enable
+	//	IEC0bits.INT0IE = 1;		// Habilitar interrupcion
 
 // Configuar Timer1. Habilitar interrupcion
 	TMR1 = 0;				// clear timer 1
@@ -106,40 +95,78 @@ int main(void)
 
 // Configurar SPI
 // PRI_PRESCAL_1_1 & SEC_PRESCAL_6_1 for ~6 MHz
-config1 = 0x0000;
-config1 = ENABLE_SCK_PIN & ENABLE_SDO_PIN & SPI_MODE16_OFF & 
-SPI_SMP_OFF & SPI_CKE_ON & SLAVE_ENABLE_OFF & 
-CLK_POL_ACTIVE_HIGH & MASTER_ENABLE_ON & SEC_PRESCAL_6_1 & PRI_PRESCAL_1_1;
-
-config2 = 0x0000;
-config2 = FRAME_ENABLE_OFF & FRAME_SYNC_OUTPUT & FRAME_POL_ACTIVE_LOW &
-FRAME_SYNC_EDGE_COINCIDE;
-
-config3 = 0x0000;
-config3 = SPI_ENABLE & SPI_IDLE_CON & SPI_RX_OVFLOW_CLR;
-
-OpenSPI1(config1, config2, config3);
-ConfigIntSPI1(SPI_INT_PRI_3 & SPI_INT_EN);   // Enable interrupt
-LATBbits.LATB2 = 0x01;  // SS idle
+   config1 = 0x0000;
+   config1 = ENABLE_SCK_PIN & ENABLE_SDO_PIN & SPI_MODE16_OFF & 
+   SPI_SMP_OFF & SPI_CKE_ON & SLAVE_ENABLE_OFF & 
+   CLK_POL_ACTIVE_HIGH & MASTER_ENABLE_ON & SEC_PRESCAL_6_1 & PRI_PRESCAL_1_1;
+   
+   config2 = 0x0000;
+   config2 = FRAME_ENABLE_OFF & FRAME_SYNC_OUTPUT & FRAME_POL_ACTIVE_LOW &
+   FRAME_SYNC_EDGE_COINCIDE;
+   
+   config3 = 0x0000;
+   config3 = SPI_ENABLE & SPI_IDLE_CON & SPI_RX_OVFLOW_CLR;
+   
+   OpenSPI1(config1, config2, config3);
+   ConfigIntSPI1(SPI_INT_PRI_3 & SPI_INT_EN);   // Enable interrupt
+   LATBbits.LATB2 = 0x01;  // SS idle
 
 
 // Activar LED que indica entrada en el lazo principal del programa
 	LATBbits.LATB13 = 0x00;		// On LED (D6)
 	while(1)
 	{
-		while (_U1RXIF == 0);			// Wait and Receive One Character
-		cmd = U1RXREG;
-		_U1RXIF = 0;					// Clear UART RX Interrupt Flag
+      if(UART_status.data != 0 && UART_status.status == 0){
+         sys_cmd = UART_status.data;
+      }
 		
-		while(!U1STAbits.TRMT);	// Esperar a que se transmita el byte antes de leer el otro de la IMU
-		U1TXREG = cmd;
+		switch(sys_cmd){
+   		case 'p': // Read the first 10 nRF24L01+ (8 bit) registers  		   
+   		   // Fill data buffer with addres to be read
+   		   cmd = R_REGISTER; // nRF24L01+ command
+   		   addr = CONFIG;
+   		   n_bytes = 1;
+   		   for(k = 0; k < BYTES_TO_READ; k++){
+      		   while(SPI_status.status != 0 ); // Wait for SPI to be idle
+   		      SPI_ReadAddr(cmd, addr, &SPI_buffer[k], n_bytes);
+   		      addr++; // Increment register address
+            }
+            sys_cmd = 0; // Reset the command as is already executed
+   		break;
+   		case 'q': // Read one nRF24L01+ (5 bit) registers
+   		   cmd = R_REGISTER; // nRF24L01+ command
+   		   addr = RX_ADDR_P0;
+   		   n_bytes = 5;
+   		   while(SPI_status.status != 0 ); // Wait for SPI to be idle
+   		   SPI_ReadAddr(cmd, addr, SPI_buffer, n_bytes);
+   		   sys_cmd = 0; // Reset the command as is already executed
+   		break;
+   		case 'r': // Read usign an specific address read command
+   		   cmd = R_RX_PL_WID; // nRF24L01+ command
+   		   addr = 0x00;   // No address with this command
+   		   n_bytes = 1;
+   		   while(SPI_status.status != 0 ); // Wait for SPI to be idle
+   		   SPI_ReadAddr(cmd, addr, SPI_buffer, n_bytes);   	
+   		   sys_cmd = 0; // Reset the command as is already executed	   
+   		break;
+		}
+		
+		// Despues de recibir los datos del nRF24L01+ enviarlos a la PC
+		if(SPI_status.status == 0 && UART_status.data != 0 && UART_status.status == 0 && sys_cmd == 0){
+   		UARTbufferSend(SPI_buffer);
+		}
+		
+		//while(!U1STAbits.TRMT);	// Esperar a que se transmita el byte antes de leer el otro de la IMU
+		//U1TXREG = cmd;
 
 	}//while(1)
 
 	return 0;
 }
 
+/**************************************************************************/
 // Funciones adicionales
+/**************************************************************************/
 char UARTbufferSend(char* c_ptr)
 {
    // Check if UART is not already transmiting
@@ -159,7 +186,9 @@ char UARTbufferSend(char* c_ptr)
 	}
 }
 
-char SPI_WriteCmd(char reg, char * d_ptr, unsigned char n_data)
+// commands that writes specific locations: W_TX_PAYLOAD, W_TX_PAYLOAD_NOACK (addr = 0x00)
+// commands that writes variable locations: W_REGISTER, W_ACK_PAYLOAD
+char SPI_WriteAddr(unsigned char cmd, unsigned char addr, char * d_ptr, unsigned char n_data)
 {
    // status: 0: Idle; 1: Sending; 2: Receiving
    if(SPI_status.status == 0){
@@ -169,12 +198,14 @@ char SPI_WriteCmd(char reg, char * d_ptr, unsigned char n_data)
       SPI_status.status = 1; // Set the sending status
       // Assert Chip select  pin
       LATBbits.LATB10 = 0x00;  //
-      WriteSPI(reg); // The first byte is the command byte
+      WriteSPI1(cmd | addr); // The first byte is the command byte
    }else{
       return 0; // Transaction in process
    }
 }
-char SPI_ReadCmd(char reg, char * d_ptr, unsigned char n_data)
+// commands that reads specific locations: R_RX_PAYLOAD, R_RX_PL_WID (addr = 0x00)
+// commands that reads variable locations: R_REGISTER
+char SPI_ReadAddr(unsigned char cmd, unsigned char addr, char * d_ptr, unsigned char n_data)
 {
    // status: 0: Idle; 1: Sending; 2: Receiving
    if(SPI_status.status == 0){
@@ -184,9 +215,9 @@ char SPI_ReadCmd(char reg, char * d_ptr, unsigned char n_data)
       SPI_status.status = 2; // Set the sending status
       // Assert Chip select  pin
       LATBbits.LATB10 = 0x00;  //
-      WriteSPI(reg); // The first byte is the command byte
+      WriteSPI1(cmd | addr); // The first byte is the command byte
    }else{
-      return 0; // Transaction in process
+      return 0; // Error: Transaction in process
    }
 }
 
@@ -195,8 +226,9 @@ void startRF_TXRX(void)
    LATBbits.LATB11 = 0x01;  // Assert the CE pin on nRF24L01+
    IEC0bits.INT0IE = 1;		// Habilitar interrupcion externa
 }
-
+/**************************************************************************/
 // Vectores de interrupcion
+/**************************************************************************/
 void __attribute__((__interrupt__, no_auto_psv)) _T1Interrupt(void)
 {
 
@@ -209,6 +241,7 @@ void __attribute__((__interrupt__, no_auto_psv)) _T1Interrupt(void)
 void __attribute__((__interrupt__, no_auto_psv)) _INT0Interrupt(void)
 {
 	// Manejo de la interrupcion externa: RB7, PIN16
+	// nRF24L01+ has sended an interruption request
 	
 	LATBbits.LATB11 = 0x00; // CE pin. desactivar la transmisión de datos del nRF24L01+
 	
@@ -235,34 +268,45 @@ void __attribute__((__interrupt__, no_auto_psv)) _U1TXInterrupt(void)
 	}
 	else
 	{
-		IEC0bits.U1TXIE = 0; // Disable UART TX interrupt until new transmision time
-		// Cambiar el estado del buffer del UART
+		IEC0bits.U1TXIE = 0; // Disable UART TX interrupt until new buffer is
+		                     // available for transmit
+		// Despues de enviar el buffer (y haber ejecutado el comando)
+		// se reinicia el comando para indicar que fue ejecutado
+		UART_status.data = 0;
 		UART_status.status = 0; // Update to idle status
 	}
 }
 
 void __attribute__((__interrupt__, no_auto_psv)) _U1RXInterrupt(void)
 {
+   _U1RXIF = 0;					// Clear UART RX Interrupt Flag
+   UART_status.data = U1RXREG;
+}
+
+void __attribute__((__interrupt__, no_auto_psv)) _SPI1ErrInterrupt (void)
+{
+   // To be filled with appropiate code
 }
 
 
-void __attribute__((__interrupt__, no_auto_psv)) _SPI1Interrupt (void)
+void __attribute__((__interrupt__, auto_psv)) _SPI1Interrupt (void)
 {
    IFS0bits.SPI1IF = 0x00; // Clear interrupt flag
    // SPI1 transfer completed interrupt 
-   switch(SPI_ststus.status)
+   switch(SPI_status.status)
    {
       case 1: // Transmiting
          // The first received byte is the STATUS register of nRF24L01+
          if(SPI_byte_counter == 0){
             SPI_status.data = ReadSPI1();
          }
+         ReadSPI1(); // Discard the received byte while transmiting
          WriteSPI1(SPI_buffer_ptr[SPI_byte_counter] & 0x00ff);
          SPI_byte_counter++;
-         if(SPI_byte_counter == SPI_max_bytes)){
+         if(SPI_byte_counter == SPI_max_bytes){
             SPI_status.status = 0;  // Transmision complete, return to idle
-            LATBbits.LATB10 = 0x01;  // SS pin
-         }         
+            LATBbits.LATB10 = 0x01;  // SS pin, release slave
+         }
          break;
       case 2: // Receiving
          // The first received byte is the STATUS register of nRF24L01+
@@ -273,9 +317,9 @@ void __attribute__((__interrupt__, no_auto_psv)) _SPI1Interrupt (void)
          }
          
          SPI_byte_counter++;
-         if(SPI_byte_counter > SPI_max_bytes)){
+         if(SPI_byte_counter > SPI_max_bytes){
             SPI_status.status = 0;  // Transmision complete, return to idle
-            LATBbits.LATB10 = 0x01;  // SS pin
+            LATBbits.LATB10 = 0x01;  // SS pin, release slave
          }else{
             WriteSPI1(0x00); // Dummy write for clocking the incoming byte
          }
